b82ff34613845037d88dc4fa8b7172fbe5d838b9
2023ITAdharanish7 <141706907+2023ITAdharanish7@users.noreply.github.com>
2025-11-30 10:06:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,122 +1,149 @@
- const gameBoard = document.getElementById("gameBoard")
-const pencil = gameBoard.getContext("2d");
-const scoreid = document.getElementById("scoreVal");
+const gameBoard = document.getElementById('gameBoard');
+const context = gameBoard.getContext('2d');
+const scoreText = document.getElementById('scoreVal');
 
-const width = gameBoard.width
-const height = gameBoard.height 
-const Foodwh = 25; 
-let yVal =0;
-let xVal =25;
-let score =0;
-let active =true
-let start = true
-let Foodx;
-let Foody;
-let snake=[
-   {x:Foodwh*3,Y:0},
-   {x:Foodwh*2,Y:0},
-   {x:Foodwh,Y:0},
-   {x:0,Y:0}     
-]
-window.addEventListener("keydown",keyPress)
+const WIDTH = gameBoard.width;
+const HEIGHT = gameBoard.height; 
+const UNIT = 25;
 
+let foodX;
+let foodY;
+let xVel = 25;
+let yVel = 0;
+let score = 0;
+let active=true;
+let started = false;
+let paused = false;
+
+let snake = [
+    {x:UNIT*3,y:0},
+    {x:UNIT*2,y:0},
+    {x:UNIT,y:0},
+    {x:0,y:0}
+];
+window.addEventListener('keydown',keyPress);
 startGame();
- function startGame(){
-    pencil.fillStyle = "#212121";
-    pencil.fillRect(0,0,width,height);   
-    createfood(); 
-   displayfood();
-   drawSnake();
-  
- }
 
-function clear(){
-     pencil.fillStyle = "#212121";
-    pencil.fillRect(0,0,width,height); 
+function startGame(){
+    context.fillStyle = '#212121';
+    //fillRect(xStart,yStart,width,height)
+    context.fillRect(0,0,WIDTH,HEIGHT);
+    createFood();
+    displayFood();
+    drawSnake();
 }
 
-function createfood(){
-    Foodx = Math.floor(Math.random()*500/25)*25;
-    Foody = Math.floor(Math.random()*500/25)*25;
+function clearBoard(){
+    context.fillStyle = '#212121';
+    //fillRect(xStart,yStart,width,height)
+    context.fillRect(0,0,WIDTH,HEIGHT);
 }
 
- function displayfood(){
- pencil.fillStyle = "red";
-    pencil.fillRect(Foodx,Foody,Foodwh,Foodwh);
- }
+function createFood(){
+    foodX = Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
+    foodY = Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
+}
+
+function displayFood(){
+    context.fillStyle = 'red';
+    context.fillRect(foodX,foodY,UNIT,UNIT)
+}
+
 function drawSnake(){
-pencil.fillStyle="blue"
-pencil.strokeStyle="#212121"
-snake.forEach((snakepart)=>{
-   pencil.fillRect(snakepart.x,snakepart.Y,Foodwh,Foodwh)
-pencil.strokeRect(snakepart.x,snakepart.Y,Foodwh,Foodwh)})
+    context.fillStyle = 'aqua';
+    context.strokeStyle = '#212121';
+    snake.forEach((snakePart) => {
+        context.fillRect(snakePart.x,snakePart.y,UNIT,UNIT)
+        context.strokeRect(snakePart.x,snakePart.y,UNIT,UNIT)
+    })
 }
 
 function moveSnake(){
-   const head ={x:snake[0].x+xVal,
-      Y:snake[0].Y+yVal};
-   snake.unshift(head)
-   if(snake[0].x==Foodx && snake[0].Y==Foody){
-      score+=1;
-      scoreid.textContent = score;
-      createfood();
-   }
-   else{
-   snake.pop()
-   }
+    const head = {x:snake[0].x+xVel,
+                    y:snake[0].y+yVel}
+    snake.unshift(head)
+    if(snake[0].x==foodX && snake[0].y==foodY){
+        score += 1;
+        scoreText.textContent = score;
+        createFood();
+    }
+    else
+        snake.pop();
+}
 
-}
 function nextTick(){
-   if(active){
-   setTimeout(()=>{
-   moveSnake();
-   clear();
-   displayfood();
-   drawSnake();
-   checkGameOver();
-   nextTick();
-   },200)
-}
+    if(active && !paused){
+        setTimeout(() => {
+            clearBoard();
+            displayFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            nextTick();
+        }, 200);
+    }
+    else if(!active){
+        clearBoard();
+        context.font = "bold 50px serif";
+        context.fillStyle = "white";
+        context.textAlign = "center";
+        context.fillText("Game Over!!",WIDTH/2,HEIGHT/2)
+    }
 }
 
 function keyPress(event){
-   if(!start){
-      start = true;
-      nextTick();
-   }
-const LEFT = 37;
-const UP = 38;
-const RIGHT = 39;
-const DOWN = 40;
+    if(!started){
+        started = true;
+        nextTick();
+    }
+    //pause when space is pressed
+    if(event.keyCode===32){
+        console.log('clicked')
+        if(paused){
+            paused = false;
+            nextTick();
+        }
+        else{
+            paused = true;
+        }
+    }
+    const LEFT = 37
+    const UP = 38
+    const RIGHT = 39
+    const DOWN = 40
 
-switch(true){
-case (event.keyCode == LEFT && xVal!=Foodwh):
-   xVal=-Foodwh;
-   yVal=0;
-   break;
+    switch(true){
+        //left key pressed and not going right
+        case(event.keyCode==LEFT  && xVel!=UNIT):
+            xVel=-UNIT;
+            yVel = 0;
+            break;
+        //right key pressed and not going left
+        case(event.keyCode==RIGHT && xVel!=-UNIT):
+            xVel=UNIT;
+            yVel=0;
+            break;
+        //Up key pressed and not going down
+        case(event.keyCode==UP && yVel!=UNIT):
+            xVel=0;
+            yVel=-UNIT;
+            break;
+        //down key pressed and not going up
+        case(event.keyCode==DOWN && yVel!=-UNIT):
+            xVel=0;
+            yVel=UNIT;
+            break;
 
-   case (event.keyCode == RIGHT && xVal!=-Foodwh):
-   xVal=Foodwh;
-   yVal=0;
-   break;
-
-   case (event.keyCode == UP && yVal!=Foodwh):
-   xVal=0;
-   yVal=-Foodwh;
-   break;
-
-   case (event.keyCode == DOWN && yVal!=-Foodwh):
-   xVal=0;
-   yVal=+Foodwh;
-   break;
-}
+    }
 }
 
 function checkGameOver(){
-   switch(true){
-      case(snake[0].x<0):
-      case(snake[0].x>=width):
-      case(snake[0].Y<0):
-      case(snake[0].Y>=height):
-   }
+    switch(true){
+        case(snake[0].x<0):
+        case(snake[0].x>=WIDTH):
+        case(snake[0].y<0):
+        case(snake[0].y>=HEIGHT):
+            active=false;
+            break;
+    }
 }
